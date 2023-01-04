584b4a9f61212a2853eb082106e61c60dae94c75
julianmiguens5 <julian.miguens5@gmail.com>
2023-01-04 18:02:43 -0300

    1.0

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WAB El Luchador</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="./img/logos/el-luchador.png" class="logo-primary" alt="el-luchador"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 customNav">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Empresa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Locales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <section class="bodyProductos">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-3">
+                    <ul class="wabUl bs-gray-500">
+
+                        <li class="wabLi wabLiTitle">CORTES VACUNOS</li>
+
+                        <li class="wabLi"><a id="prod56" href="productos-piala-56">ARAÑITA</a></li>
+
+                        <li class="wabLi"><a id="prod7" href="productos-piala-7">ASADO - COSTILLA DE TERNERA</a></li>
+
+                        <li class="wabLi"><a id="prod45" href="productos-piala-45">ASADO DE NOVILLITO</a></li>
+
+                        <li class="wabLi"><a id="prod29" href="productos-piala-29">BRAZUELO C HUESO</a></li>
+
+                        <li class="wabLi"><a id="prod30" href="productos-piala-30">BRAZUELO S HUESO</a></li>
+
+                        <li class="wabLi"><a id="prod2" href="productos-piala-2">CABEZA DE LOMO / BOLA DE LOMO</a></li>
+
+                        <li class="wabLi"><a id="prod52" href="productos-piala-52">CIMA DE TERNERA</a></li>
+
+                        <li class="wabLi"><a id="prod6" href="productos-piala-6">COLITA DE CUADRIL / PUNTA DE VACIO</a></li>
+
+                        <li class="wabLi"><a id="prod10" href="productos-piala-10">COSTELETA CHICA</a></li>
+
+                        <li class="wabLi"><a id="prod25" href="productos-piala-25">COSTELETAS GRANDES</a></li>
+
+                        <li class="wabLi"><a id="prod3" href="productos-piala-3">CUADRIL</a></li>
+
+                        <li class="wabLi"><a id="prod43" href="productos-piala-43">ENTRAÑA FINA</a></li>
+
+                        <li class="wabLi"><a id="prod31" href="productos-piala-31">ENTRECOT COMUN / ROAST BEEF</a></li>
+
+                        <li class="wabLi"><a id="prod32" href="productos-piala-32">ENTRECOT ESPECIAL / BIFE DE CHORIZO</a></li>
+
+                        <li class="wabLi"><a id="prod8" href="productos-piala-8">FALDA</a></li>
+
+                        <li class="wabLi"><a id="prod4" href="productos-piala-4">JAMON CUADRADO</a></li>
+
+                        <li class="wabLi"><a id="prod5" href="productos-piala-5">JAMON REDONDO / PECETO</a></li>
+
+                        <li class="wabLi"><a id="prod9" href="productos-piala-9">LOMO</a></li>
+
+                        <li class="wabLi"><a id="prod36" href="productos-piala-36">MAR DEL PLATA</a></li>
+
+                        <li class="wabLi"><a id="prod27" href="productos-piala-27">MATAMBRE DE TERNERA</a></li>
+
+                        <li class="wabLi"><a id="prod1" href="productos-piala-1">NALGA</a></li>
+
+                        <li class="wabLi"><a id="prod26" href="productos-piala-26">OSOBUCO</a></li>
+
+                        <li class="wabLi"><a id="prod35" href="productos-piala-35">PALOMITA</a></li>
+
+                        <li class="wabLi"><a id="prod329" href="productos-piala-329">PATA TERNERA 5KG/10KG</a></li>
+
+                        <li class="wabLi"><a id="prod47" href="productos-piala-47">PATA TERNERA 7KG</a></li>
+
+                        <li class="wabLi"><a id="prod11" href="productos-piala-11">PICADA COMUN</a></li>
+
+                        <li class="wabLi"><a id="prod12" href="productos-piala-12">PICADA ESPECIAL</a></li>
+
+                        <li class="wabLi"><a id="prod37" href="productos-piala-37">PUCHERO COMUN</a></li>
+
+                        <li class="wabLi"><a id="prod34" href="productos-piala-34">TAPA DE ASADO</a></li>
+
+                        <li class="wabLi"><a id="prod59" href="productos-piala-59">TAPA DE CUADRIL / PICAÑA</a></li>
+
+                        <li class="wabLi"><a id="prod33" href="productos-piala-33">TAPA DE NALGA</a></li>
+
+                        <li class="wabLi"><a id="prod28" href="productos-piala-28">VACIO</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-12 col-md-9">
+                    <div class="grillaItems">
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="BUSCAR" aria-label="Search">
+                            <button class="btn btn-outline-light" type="submit">BUSCAR</button>
+                        </form>
+                        <ul class="items">
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                            <li class="itemList">
+
+
+                                <img src="https://piala.com.ar/sk-admin/browser/archivos/productos/56/grande_777065995.jpg" width="205">
+                                <br>
+                                <div class="middle">
+                                <a href="#" class="add2cart" data-id="56">
+                                        <i class="bi bi-cart"></i>
+                                    </a>
+                                </div>
+                                <div class="name">ARAÑITA</div>
+                                <div class="price">$999.99 / Kg</div>
+                                
+
+
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <script src="./scss/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
